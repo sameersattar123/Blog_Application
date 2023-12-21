0fbeea9e17a0722bcd903cc100eb21d62445ef41
Sameer <sameersattar1111@gmail.com>
2023-12-21 09:45:20 +0500

    functionalilty

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import {useDispatch} from "react-redux"
+import authSlice, { login, logout } from './store/authSlice';
+import authService from "./appwrite/auth"
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import {Outlet} from 'react-router-dom'
 const App = () => {
-  // console.log(import.meta.env.VITE_APPWRITE_URL)
-  // console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID)
-  return (
-    <div>App</div>
-  )
+  const [ loading , setLoading ] = useState(true)
+  const dispatch = useDispatch();
+  
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+          dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  
+ return !loading ? (
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+  <div className='w-full block'>
+    <Header/>
+    <Outlet/>
+    <Footer/>
+    </div>
+    </div>
+ ) : null
+  
 }
 
 export default App
