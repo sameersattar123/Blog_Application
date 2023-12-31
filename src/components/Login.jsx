769d35ce2cec6e3f5441cff3e771f96899542458
Sameer <sameersattar1111@gmail.com>
2023-12-31 23:40:17 +0500

    depencies

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { Link , useNavigate} from 'react-router-dom'
+import {login as authLogin} from "../store/authSlice"
+import {useForm} from "react-hook-form"
+import { useState } from 'react'
+import authService from "../appwrite/auth"
+import Button from './Button'
+import Logo from './Logo'
+import Input from './Input'
 
 const Login = () => {
   return (
