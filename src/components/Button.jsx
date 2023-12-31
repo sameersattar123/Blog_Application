9fbde45d28cb946faf9f04977891a463c0a71d55
Sameer <sameersattar1111@gmail.com>
2023-12-31 22:04:01 +0500

    components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
-    console.log("sameer sattar")
+const Button = ({
+  children,
+  type = "button",
+  bgColor = "bg-blue-600",
+  textColor = "text-white",
+  className = "",
+  ...props
+}) => {
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      className={`px-4 py-2 rounded-lg ${className} ${textColor} ${bgColor}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useDispatch } from "react-redux"
+import  {logout} from "../../store/authSlice"
+import authService  from "../../appwrite/auth"
 
 const LogoutBtn = () => {
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    authService.logout().then(() => {
+      dispatch(logout())
+    })
+  }
+
   return (
-    <div>LogoutBtn</div>
+    <button className='inline-block py-2 px-6 duration-200 hover:bg-blue-100 rounded-full'> 
+      Logout
+    </button>
   )
 }
 
