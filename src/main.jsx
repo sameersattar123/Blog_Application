361bcea543f38e416d3a58d02c1a3879746910d4
Sameer <sameersattar1111@gmail.com>
2024-01-04 02:10:12 +0500

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,78 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AuthLayout from "./components/AuthLayout.jsx"
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import AllPost from "./pages/AllPost.jsx";
+import EditPost from "./pages/EditPost.jsx";
+import AddPost from "./pages/AddPost.jsx";
+import Post from "./pages/Post.jsx";
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <App/>,
+    children : [
+      {
+        path:"/",
+        element : <Home/>
+      },
+      {
+        path : "/login",
+        element : (
+          <AuthLayout authenication={false}>
+            <Login/>
+          </AuthLayout>
+        )
+      },
+      {
+        path : "/signup",
+        element : (
+          <AuthLayout authenication={false}>
+            <Signup/>
+          </AuthLayout>
+        )
+      },
+      {
+        path : "/all-posts",
+        element : (
+          <AuthLayout authenication>
+            <AllPost/>
+          </AuthLayout>
+        )
+      },
+      {
+        path : "/add-post",
+        element : (
+          <AuthLayout authenication>
+            <AddPost/>
+          </AuthLayout>
+        )
+      },
+      {
+        path : "/edit-post/:slug",
+        element : (
+          <AuthLayout authenication>
+            <EditPost/>
+          </AuthLayout>
+        )
+      },
+      {
+        path : "/post/:slug",
+        element : (         
+            <Post/>
+        )
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  
+
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>
 );
