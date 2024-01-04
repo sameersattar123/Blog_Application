8f9269097022d3c837aa6c2d2e1f2e4d2b7564ea
Sameer <sameersattar1111@gmail.com>
2024-01-04 23:29:46 +0500

    component

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Container from "../components/Container/Container";
 import PostForm from "../components/PostForm/PostForm";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import appwriteService from "../appwrite/config";
+import Button from "../components/Button";
+import parse from "html-react-parser";
 
 const Post = () => {
   const navigate = useNavigate();
@@ -42,7 +44,28 @@ const Post = () => {
   return (
     <div className="py-8">
       <Container>
-        
+        <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
+          <img
+            src={appwriteService.getFilePreview(post.featuredImage)}
+            alt={post.title}
+            className="rounded-xl"
+          />
+          {isAuthor && (
+            <div className="absolute right-6 top-6">
+              <Link to={`/edit-post/${post.$id}`}>
+                <Button bgColor="bg-green-500" className="mr-3">
+                  Edit
+                </Button>
+                <Button bgColor="bg-red-500" onClick={deletePost} >Delete</Button>
+              </Link>
+            </div>
+          )}
+        </div>
+
+        <div className="w-full mb-6">
+          <h1 className="text-2xl font-bold">{post.title}</h1>
+        </div>
+        <div className="browser-css">{parse(post.content)}</div>
       </Container>
     </div>
   );
