6ab2e982ea8e84e165986ee4e9d771f1883b9a2d
Sameer <sameersattar1111@gmail.com>
2024-01-04 22:38:23 +0500

    component

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,10 @@ const App = () => {
   <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
   <div className='w-full block'>
     <Header/>
-    <Outlet/>
-    {/* <Footer/> */}
+    <main>
+   TODO: <Outlet/>
+    </main>
+    <Footer/>
     </div>
     </div>
  ) : null
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,15 +1,51 @@
-import React from 'react'
-import Container from "../components/Container/Container"
-import PostForm from '../components/PostForm/PostForm'
+import React, { useEffect, useState } from "react";
+import Container from "../components/Container/Container";
+import PostForm from "../components/PostForm/PostForm";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import appwriteService from "../appwrite/config";
 
 const Post = () => {
+  const navigate = useNavigate();
+
+  const [post, setPost] = useState(null);
+
+  const { slug } = useParams();
+
+  const userData = useSelector((state) => state.auth.userData);
+
+  const isAuthor = post && userData ? post.userId === userData.$id : false;
+
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) {
+          setPost(post);
+        } else {
+          navigate("/");
+        }
+      });
+    } else {
+      navigate("/");
+    }
+  }, [slug, navigate]);
+
+  const deletePost = () => {
+    appwriteService.deletePost(post.$id).then((status) => {
+      if (status) {
+        appwriteService.deleteFile(post.featuredImage);
+        navigate("/");
+      }
+    });
+  };
+
   return (
-    <div className='py-8'>
+    <div className="py-8">
       <Container>
-        <PostForm/>
+        
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
